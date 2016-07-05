0507511c510e311d01c161c84415da18aa9aa73a
homoefficio <homo.efficio@gmail.com>
2016-07-06 08:44:12 +0900

    확장 함수 리시버의 private에 접근 가능한가

--- src/004-extension-function.kt
+++ src/004-extension-function.kt
@@ -55,4 +55,21 @@ fun main(args: Array<String>) {
         msg(B())  // 다형성이 있다면 "This is B"가 출력되겠지만 확장함수는 정적 바인딩이므로 "This is A"가 출력됨
     }
     extensionFunctionStaticBinding()
+
+    // 확장 함수는 리시버의 private에 접근 가능할까?
+    fun extensionFunctionAccessingPrivatePropertyOfReceiver() {
+
+        println("===== extension function accessing private =====")
+
+        class A {
+            val a = "public"
+            private val b = "private"
+        }
+
+        fun A.toUpper1(): String = this.a.toUpperCase()
+//        fun A.toUpper2(): String = this.b.toUpperCase()  // 컴파일 에러. 즉 확장 함수는 리시버의 private에 접근 불가
+
+        println(A().toUpper1())
+    }
+    extensionFunctionAccessingPrivatePropertyOfReceiver()
 }
